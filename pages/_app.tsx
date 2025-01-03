a185eee77f372cb2749fb581076868a4cfaf1f5b
Vimal Thanikachalam <vimaleurakaa@gmail.com>
2025-01-03 16:38:40 +0530

    updated website responsive

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,23 @@ import { Header } from "@/components/Header";
 import { LayoutWrapper } from "@/components/Layout";
 import "@/styles/index.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main>
-      <Header />
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
-    </main>
+    <>
+      <Head>
+        <title>Sidvin | Interactive Videos</title>
+        <meta name="description" content="Sidvin | Interactive Videos" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <main>
+        <Header />
+        <LayoutWrapper>
+          <Component {...pageProps} />
+        </LayoutWrapper>
+      </main>
+    </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,7 @@ import { Head, Html, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Sidvin | Interactive Videos</title>
-        <meta name="description" content="Sidvin | Interactive Videos" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.png" />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
